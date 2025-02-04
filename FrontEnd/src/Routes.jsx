241337c970f351e7dd8be980d575f648d715ace6
KushanSingh-Kalvium <kushansingh890@gmail.com>
2025-02-04 14:38:49 +0530

    Renamed

--- FrontEnd/src/Routes.jsx
+++ FrontEnd/src/Routes.jsx
@@ -1,5 +1,5 @@
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
 import Home from "./pages/Home";
-import CreateProduct from "./pages/Product.jsx";
+import CreateProduct from "./pages/Create_Product.jsx";
 export {LoginPage, SignupPage, Home, CreateProduct};
