f88063932d311e696dcbf0a98f9240af2ddd4db7
KushanSingh-Kalvium <kushansingh890@gmail.com>
2025-02-12 14:32:52 +0530

    Milestone 16 Completed

--- FrontEnd/src/Routes.jsx
+++ FrontEnd/src/Routes.jsx
@@ -3,5 +3,14 @@ import SignupPage from "./pages/Signup.jsx";
 import Home from "./pages/Home.jsx";
 import CreateProduct from "./pages/Create_Product.jsx";
 import MyProducts from "./pages/myProducts.jsx";
-import Cart from "./pages/cart.jsx"
-export { SignupPage, LoginPage, Home, CreateProduct, MyProducts,Cart};
+import Cart from "./pages/cart.jsx";
+import ProductDetails from "./pages/productDetails.jsx";
+export {
+  SignupPage,
+  LoginPage,
+  Home,
+  CreateProduct,
+  MyProducts,
+  Cart,
+  ProductDetails,
+};
